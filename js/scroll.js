54f018d2c3906581418633739812a46037c18c74
Dmytro <harss320@gmail.com>
2022-12-01 04:25:01 +0200

    Update folders

--- js/scroll.js
+++ js/scroll.js
@@ -7,16 +7,6 @@ window.addEventListener('scroll', () =>{
         toTop.classList.remove('active')
     )
 })
-
-/* function showMenu(){
-    document.querySelector('nav').classList.add('active-menu')
-}
-function closeMenu(){
-    document.querySelector('nav').classList.add('animate__flipOutX')
-    document.querySelector('nav').delay(2000).classList.remove('active-menu')
-}
-document.querySelector('nav').classList.remove('animate__flipOutX') */
-
 function topFunction(){
     document.documentElement.scrollTop = 0;
 }
