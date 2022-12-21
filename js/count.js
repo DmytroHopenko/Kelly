6ef432ff53d2faa00a5e6bde548d189acb76de9d
Dmytro <harss320@gmail.com>
2022-12-21 23:04:31 +0200

    Big update
    Big update

---
+++ js/count.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.count').each(function(){
+        $(this).prop('Counter', 0).animate({
+            Counter:$(this).text()
+        },{
+            duration: 2000,
+            easing: 'swing',
+            step:(function(now){
+                $(this).text(Math.ceil(now));
+            })
+        });
+    });
+});
