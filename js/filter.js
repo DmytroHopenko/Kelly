b0f29e316ab45a21602f9a3d1649be5195920050
Dmytro <harss320@gmail.com>
2023-07-23 21:39:18 +0300

    Portfolio updated

--- js/filter.js
+++ js/filter.js
@@ -1,48 +1,46 @@
-$(document).ready(function(){
-    $('.all').click(function(){
-        $('.web').removeClass('active-btn');
-        $('.app').removeClass('active-btn');
-        $('.card').removeClass('active-btn');
-        $(this).addClass('active-btn');
+$(document).ready(function () {
+  $(".all").click(function () {
+    $(".web").removeClass("active-btn");
+    $(".app").removeClass("active-btn");
+    $(".card").removeClass("active-btn");
+    $(this).addClass("active-btn");
 
-        $('.img_app').show();
-        $('.img_web').show();
-        $('.img_card').show();
-        position();
+    $(".img_app").show();
+    $(".img_web").show();
+    $(".img_card").show();
+    position();
+  });
+  $(".app").click(function () {
+    $(".all").removeClass("active-btn");
+    $(".card").removeClass("active-btn");
+    $(".web").removeClass("active-btn");
+    $(this).addClass("active-btn");
 
-    })
-    $('.app').click(function(){
-        $('.all').removeClass('active-btn');
-        $('.card').removeClass('active-btn');
-        $('.web').removeClass('active-btn');
-        $(this).addClass('active-btn');
+    $(".img_app").removeAttr("style").show();
+    $(".img_web").hide();
+    $(".img_card").hide();
+    show_img();
+  });
+  $(".card").click(function () {
+    $(".all").removeClass("active-btn");
+    $(".app").removeClass("active-btn");
+    $(".web").removeClass("active-btn");
+    $(this).addClass("active-btn");
 
-        $('.img_app').removeAttr('style').show();
-        $('.img_web').hide();
-        $('.img_card').hide();
-        show_img();
-        
-    })
-    $('.card').click(function(){
-        $('.all').removeClass('active-btn');
-        $('.app').removeClass('active-btn');
-        $('.web').removeClass('active-btn');
-        $(this).addClass('active-btn');
+    $(".img_card").removeAttr("style").show();
+    $(".img_web").hide();
+    $(".img_app").hide();
+    show_img();
+  });
+  $(".web").click(function () {
+    $(".all").removeClass("active-btn");
+    $(".app").removeClass("active-btn");
+    $(".card").removeClass("active-btn");
+    $(this).addClass("active-btn");
 
-        $('.img_card').removeAttr('style').show();
-        $('.img_web').hide();
-        $('.img_app').hide();
-        show_img();
-    })
-    $('.web').click(function(){
-        $('.all').removeClass('active-btn');
-        $('.app').removeClass('active-btn');
-        $('.card').removeClass('active-btn');
-        $(this).addClass('active-btn');
-
-        $('.img_web').removeAttr('style').show();
-        $('.img_app').hide();
-        $('.img_card').hide();
-        show_img();
-    })
+    $(".img_web").removeAttr("style").show();
+    $(".img_app").hide();
+    $(".img_card").hide();
+    show_img();
+  });
 });
